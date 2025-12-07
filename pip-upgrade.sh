146122b23bd8737571e06c179a3c08c01bb2e52f
André Groen <andrephgroen@gmail.com>
2025-12-07 19:45:17 +0100

    20251207 replacement of temporary file to directory ./ws/

--- pip-upgrade.sh
+++ pip-upgrade.sh
@@ -7,7 +7,7 @@ pip -V
 export PIPLIST=$(expr $(pip list | cut -f 1 -d" " | wc -l) - 2)
 echo "Number of software packages: "${PIPLIST}
 echo "Create temporary file ~/ws/pip-list.txt"
-pip list | cut -f 1 -d" " | tail -n ${PIPLIST} > ~/pip-list.txt
+pip list | cut -f 1 -d" " | tail -n ${PIPLIST} > ~/ws/pip-list.txt
 pip install --upgrade -r ~/ws/pip-list.txt
 echo "Removing temporary file ~/ws/pip-list.txt"
 rm ~/ws/pip-list.txt 
