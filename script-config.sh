41550974d19c014491b4e84fb282c6e18d2d50e6
André Groen <andrephgroen@gmail.com>
2025-12-10 21:32:49 +0100

    20251210 changes to script-config.sh

--- script-config.sh
+++ script-config.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 clear
 echo "Cleaning previous link files..."
-sudo rm /usr/local/bin/pip-*.sh /usr/local/bin/sdkman-*.sh /usr/local/bin/my-*.sh
-export NUMSCRIPTS=$(ls ./pip-*.sh ./sdkman-*.sh ./my-*.sh | wc -l)
+sudo rm /usr/local/bin/pip-* /usr/local/bin/sdkman-* /usr/local/bin/my-* /usr/local/bin/scan-*
+export NUMSCRIPTS=$(ls ./pip-*.sh ./sdkman-*.sh ./my-*.sh ./scan-*.sh | wc -l)
 echo "Number of run-scripts: "${NUMSCRIPTS}
 echo "Creating temporary list file..."
-ls ./pip-*.sh ./sdkman-*.sh ./my-*.sh > ./script-list.txt
+ls pip-* sdkman-* my-* scan-* > ./script-list.txt
+cat ./script-list.txt | cut -f 1 -d"." > ./script-list2.txt
 echo "Creating link files..."
-for SCRIPT in $(cat ./script-list.txt) ;
+for SCRIPT in $(cat ./script-list2.txt) ;
 do
 	echo "Creating link for: "${SCRIPT}
-	sudo ln -s $(pwd)/${SCRIPT} /usr/local/bin/${SCRIPT}
+	sudo ln -s $(pwd)/${SCRIPT}.sh /usr/local/bin/${SCRIPT}
 done;
 echo "Deleting temporary list file..."
 rm ./script-list.txt
+rm ./script-list2.txt
 echo "Ready!"
