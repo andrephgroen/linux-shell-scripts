9383fa24624f895e9af5063a2d13fdaf26847317
Andre Groen <andrephgroen@msn.com>
2026-04-04 16:47:29 +0200

    20260404 update 'my-script-files.sh'

--- my-script-files.sh
+++ my-script-files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
 export LOCALBIN='/usr/local/bin'
-export NUMOFSCRIPTS=$(expr $(ls -ls ${LOCALBIN} | grep -i 'linux-shell-scripts' | cut -f 10,11,12,13 wc -l))
+export NUMOFSCRIPTS=$(expr $(ls -ls ${LOCALBIN} | grep -i 'linux-shell-scripts' | wc -l))
 echo "Number of current available scripts: "${NUMOFSCRIPTS}
 echo "Script files:"
-ls -ls ${LOCALBIN} | grep -i 'linux-shell-scripts' | cut -f 10,11,12,13 -d" " | sort
+ls -ls ${LOCALBIN} | grep -i 'linux-shell-scripts' | cut -f '10,11,12,13' -d" " | sort
 echo "Ready!"
